a14512368554172f8306e78bec1ffc0a7f72d77a
yildizemre2021 <yildiz.emre.2021@gmail.com>
2022-05-01 21:23:54 +0300

    Versiyon 1.0

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Random, Rasgele demektir.
+
+Random rasgele = new Random();
+
+int sayi = rasgele.Next(1, 100);
+Console.Clear();
+while (true)
+{
+    Console.Write("Sayi tahmininizi yapin: ");
+    int tahmin = int.Parse(Console.ReadLine());
+
+    if (tahmin < sayi)
+    {
+        Console.WriteLine("Tahmininiz sayidan kucuk!");
+    }
+    else if (tahmin > sayi)
+    {
+        Console.WriteLine("Tahmininiz sayidan buyuk!");
+    }
+    else if (tahmin == sayi)
+    {
+        Console.WriteLine("Tebrikler " + sayi + " sayisini buldunuz!");
+        break;
+    }
+}
+Console.WriteLine("Program bitti.");
